ae4d89a675fbdf1a86f4ef5a817c9eff95724282
helio.rodrigues <helio.rodrigues>
2022-04-27 01:57:06 -0400

    Adicionando morgan

--- Game/index.js
+++ Game/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  console.log("Requisição " + req.method + " " + req.url);
+  next();
+});
+
 app.get("/", function (req, res) {
   res.send("Hello world!");
+});
+
+app.post("/", function (req, res) {
+  res.send("Post");
 });
 
 app.listen(3000, function () {
